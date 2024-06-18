e621ffd09a2a769aeda10631036d537a7219e195
wanghanhao <3252913677@qq.com>
2024-06-18 23:23:22 +0800

    feat:初次启动项目

---
+++ op-system-center/src/main/java/com/only4play/system/SystemCenterApplication.java
@@ -0,0 +1,14 @@
+package com.only4play.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SystemCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SystemCenterApplication.class,args);
+    }
+}
