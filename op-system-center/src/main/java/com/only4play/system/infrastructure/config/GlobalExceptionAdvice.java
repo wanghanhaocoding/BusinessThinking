6b757de60b8726d2a0211a7d6d8c1c032941f52f
wanghanhao <3252913677@qq.com>
2024-06-22 23:02:31 +0800

    feat:领域开发产品搭建

--- op-system-center/src/main/java/com/only4play/system/infrastructure/config/GlobalExceptionAdvice.java
+++ op-system-center/src/main/java/com/only4play/system/infrastructure/config/GlobalExceptionAdvice.java
@@ -1,8 +1,10 @@
 package com.only4play.system.infrastructure.config;
 
+import com.google.common.collect.Iterables;
 import com.only4play.common.constants.CodeEnum;
 import com.only4play.common.exception.BusinessException;
 import com.only4play.common.exception.SystemException;
+import com.only4play.common.exception.ValidationException;
 import com.only4play.common.model.JsonObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -31,6 +33,13 @@ public class GlobalExceptionAdvice {
   public JsonObject handleException(Exception e){
     log.error("系统异常",e);
     return JsonObject.fail(CodeEnum.SystemError);
+  }
+  @ExceptionHandler(ValidationException.class)
+  public JsonObject handleValidateException(ValidationException validationException){
+    if (Iterables.size(validationException.getResult()) > 0){
+      return JsonObject.fail(validationException.getResult().get(0).getMessage());
+    }
+    return JsonObject.fail(CodeEnum.ParamSetIllegal.getName());
   }
 
 
